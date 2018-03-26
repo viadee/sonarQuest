75c78c14a1e6926190d419f7757461d16a51dcc6
Tamara Gunkel <tamara.gunkel@viadee.de>
2018-03-26 11:28:13 +0200

    Fixes #7 choose image for quest

--- sonarQuest-frontend/src/app/pages/gamemaster-page/components/gamemaster-quest/components/gamemaster-quest-create/gamemaster-quest-create.component.ts
+++ sonarQuest-frontend/src/app/pages/gamemaster-page/components/gamemaster-quest/components/gamemaster-quest-create/gamemaster-quest-create.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { Quest, Task } from "../../../../../../Interfaces/Quest";
-import { WorldService } from "../../../../../../services/world.service";
-import { World } from "../../../../../../Interfaces/World";
-import { MatDialog, MatDialogRef } from "@angular/material";
-import { GamemasterAddFreeTaskComponent } from "./components/gamemaster-add-free-task/gamemaster-add-free-task.component";
-import { GamemasterSuggestTasksComponent } from "./components/gamemaster-suggest-tasks/gamemaster-suggest-tasks.component";
-import { QuestService } from "../../../../../../services/quest.service";
-import { GamemasterQuestComponent } from "app/pages/gamemaster-page/components/gamemaster-quest/gamemaster-quest.component";
-import { TaskService } from "../../../../../../services/task.service";
+import {Component, OnInit} from '@angular/core';
+import {Quest, Task} from "../../../../../../Interfaces/Quest";
+import {WorldService} from "../../../../../../services/world.service";
+import {World} from "../../../../../../Interfaces/World";
+import {MatDialog, MatDialogRef} from "@angular/material";
+import {GamemasterAddFreeTaskComponent} from "./components/gamemaster-add-free-task/gamemaster-add-free-task.component";
+import {GamemasterSuggestTasksComponent} from "./components/gamemaster-suggest-tasks/gamemaster-suggest-tasks.component";
+import {QuestService} from "../../../../../../services/quest.service";
+import {GamemasterQuestComponent} from "app/pages/gamemaster-page/components/gamemaster-quest/gamemaster-quest.component";
+import {TaskService} from "../../../../../../services/task.service";
 
 @Component({
   selector: 'app-gamemaster-quest-create',
@@ -23,27 +23,31 @@ export class GamemasterQuestCreateComponent implements OnInit {
   story: string;
   currentWorld: World;
   tasks: Task[] = [];
+  images: any[];
+  selectedImage: string;
 
-  constructor(
-    private questService: QuestService,
-    private taskService: TaskService,
-    private dialog: MatDialog,
-    private worldService: WorldService,
-    private dialogRef: MatDialogRef<GamemasterQuestComponent>
-  ) { }
+  constructor(private questService: QuestService,
+              private taskService: TaskService,
+              private dialog: MatDialog,
+              private worldService: WorldService,
+              private dialogRef: MatDialogRef<GamemasterQuestComponent>) {
+  }
 
   ngOnInit() {
     this.worldService.currentWorld$.subscribe(world => this.currentWorld = world);
+    this.loadImages();
+    this.selectedImage = "http://via.placeholder.com/200x200";
   }
 
   createQuest() {
-    if (this.title && this.gold && this.xp && this.story && this.currentWorld && (this.tasks.length != 0)) {
+    if (this.title && this.gold && this.xp && this.story && this.selectedImage && this.currentWorld && (this.tasks.length != 0)) {
       let quest = {
         title: this.title,
         gold: this.gold,
         xp: this.xp,
         story: this.story,
-        world: this.currentWorld,
+        image: this.selectedImage,
+        world: this.currentWorld
       }
       this.questService.createQuest(quest).then((quest) => {
         if (quest.id) {
@@ -64,16 +68,16 @@ export class GamemasterQuestCreateComponent implements OnInit {
 
 
   addFreeTask() {
-    this.dialog.open(GamemasterAddFreeTaskComponent, { data: [this.currentWorld, this.tasks] })
+    this.dialog.open(GamemasterAddFreeTaskComponent, {data: [this.currentWorld, this.tasks]})
       .afterClosed().subscribe(result => {
-        if (result) {
-          this.tasks.push(result)
-        }
-      });
+      if (result) {
+        this.tasks.push(result)
+      }
+    });
   }
 
   suggestTasks() {
-    this.dialog.open(GamemasterSuggestTasksComponent, { data: [this.currentWorld, this.tasks] }).afterClosed().subscribe(result => {
+    this.dialog.open(GamemasterSuggestTasksComponent, {data: [this.currentWorld, this.tasks]}).afterClosed().subscribe(result => {
       if (result) {
         this.tasks = this.tasks.concat(result)
       }
@@ -90,11 +94,20 @@ export class GamemasterQuestCreateComponent implements OnInit {
       return a + b;
     }, 0);
   }
+
   calculateXpAmountOfTasks(): number {
     return this.tasks.map(task => task.xp).reduce(function (a, b) {
       return a + b;
     }, 0);
   }
 
+  loadImages() {
+    this.images = [];
 
+    for (let i = 0; i < 15; i++) {
+      this.images[i] = {};
+      this.images[i].src = "assets/images/quest/hero" + (i + 1) + ".jpg";
+      this.images[i].name = "hero" + (i + 1);
+    }
+  }
 }
