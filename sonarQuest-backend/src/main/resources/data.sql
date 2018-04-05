ae9820d03d8b3b5377ae8ed220abfd299a6487f4
Christoph-Meyer <mec@viadee.de>
2018-04-05 18:06:05 +0200

    https://github.com/viadee/sonarQuest/issues/14 testdata expanded

--- sonarQuest-backend/src/main/resources/data.sql
+++ sonarQuest-backend/src/main/resources/data.sql
@@ -3,40 +3,52 @@ INSERT INTO Level (name, min, max) VALUES
   ('2', 11, 30);
 
 INSERT INTO Avatar_Class (name) VALUES
-  ('Magier'),
-  ('Krieger');
+  ('Magician'),
+  ('Warrior'),
+  ('Adventurer'),
+  ('Thief'),
+  ('Bard');
 
 INSERT INTO Avatar_Race (name) VALUES
-  ('Mensch'),
-  ('Zwerg');
+  ('Human'),
+  ('Dwarf'),
+  ('Barbarian'),
+  ('Elf');
 
 INSERT INTO Skill (name,type,value) VALUES
-  ('Magier','GOLD',2),
-  ('Krieger','XP',2),
-  ('MehrGold','GOLD',1),
-  ('MehrXp','XP',1);
+  ('Staffwielding','GOLD',2),
+  ('Swordfighting','XP',2),
+  ('Golden Hands','GOLD',1),
+  ('Seasoned','XP',1),
+  ('Musical', 'GOLD',3);
 
 INSERT INTO Avatar_Class_Skill (avatar_class_id,skill_id) VALUES
   (1,1),
-  (2,2);
+  (2,2),
+  (3,4),
+  (4,3),
+  (5,5);
 
 INSERT INTO Artefact (name,icon,price,level_id) VALUES
-  ('Schwert','ra-sword',50,1),
-  ('Schild','ra-fire-shield',200,2);
+  ('Shortsword','ra-sword',50,1),
+  ('Small Shield','ra-fire-shield',200,2),
+  ('Blue Dagger of the Song','ra-sword',150,2),
+  ('Axe of Doom','ra-axe',75,1);
 
 INSERT INTO Artefact_Skill (artefact_id,skill_id) VALUES
   (1,3),
-  (2,4);
+  (2,4),
+  (3,5);
 
 INSERT INTO Developer (username, gold, xp, level_id,picture,about_me,avatar_class_id,avatar_race_id) VALUES
-  ('sonarHero123', 10, 5, 1,'assets/images/quest/hero2.jpg','Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et',1,2),
-  ('sonarQuester123', 30, 20, 2,'assets/images/quest/hero4.jpg','Ich bin der Allerbeste!',2,1),
-  ('sonarWarrior', 18, 15, 2,'assets/images/quest/hero6.jpg','BADABOOOOM',2,1);
+  ('Eddie Tor', 10, 5, 1,'assets/images/quest/hero13.jpg','Quick with the keys as well as the daggers, Eddie knows hidden paths to chambers of wisdom unknown!',4,2),
+  ('Ringo Rockstar', 30, 20, 2,'assets/images/quest/hero9.jpg','The mighty barbarian has the strength and the tools to do the job. Brute force ftw!',2,3),
+  ('Mike Magician', 18, 15, 2,'assets/images/quest/hero1.jpg','Raised in the Lands of the North, wisdomey and experienced, this talented Magician will find a solution for everything. And sometimes it might really look like magic!',1,1);
 
 INSERT INTO Developer_Artefact (developer_id,artefact_id) VALUES
-  (1,1),
-  (2,1),
-  (2,2);
+  (1,3),
+  (2,4),
+  (3,1);
 
 
 
