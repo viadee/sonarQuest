1687657ab4b00bf83b2646bceeee730a74cd78c8
Christoph-Meyer <mec@viadee.de>
2018-04-16 21:17:04 +0200

    Bugfix hashcode/equals

--- sonarQuest-backend/src/main/java/com/viadee/sonarQuest/entities/Artefact.java
+++ sonarQuest-backend/src/main/java/com/viadee/sonarQuest/entities/Artefact.java
@@ -1,9 +1,19 @@
 package com.viadee.sonarQuest.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import javax.persistence.*;
 import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "Artefact")
@@ -21,15 +31,15 @@ public class Artefact {
 
     @Column(name = "price")
     private Long price;
-    
+
     @Column(name = "quantity")
     private Long quantity;
 
     @Column(name = "description")
     private String description;
-    
+
     @ManyToOne
-    @JoinColumn(name="level_id")
+    @JoinColumn(name = "level_id")
     private Level minLevel;
 
     @ManyToMany(cascade = CascadeType.ALL)
@@ -51,21 +61,22 @@ public class Artefact {
     }
 
     public Artefact(String name, Long price, Long quantity, String description) {
-        this.name 		 = name;
-        this.price 		 = price;
-        this.quantity 	 = quantity;
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
         this.description = description;
     }
 
     public Artefact(String name, String icon, Long price, Long quantity, String description) {
-        this.name 		 = name;
-        this.icon 		 = icon;
-        this.price 		 = price;
-        this.quantity 	 = quantity;
+        this.name = name;
+        this.icon = icon;
+        this.price = price;
+        this.quantity = quantity;
         this.description = description;
     }
 
-    public Artefact(String name, String icon, Long price, Level minLevel, List<Skill> skills, List<Developer> developers) {
+    public Artefact(String name, String icon, Long price, Level minLevel, List<Skill> skills,
+            List<Developer> developers) {
         this.name = name;
         this.icon = icon;
         this.price = price;
@@ -73,22 +84,21 @@ public class Artefact {
         this.skills = skills;
         this.developers = developers;
     }
-    
 
-	public Artefact(Long id, String name, String icon, Long price, Long quantity, String description, Level minLevel,
-			List<Skill> skills, List<Developer> developers) {
-		this.id = id;
-		this.name = name;
-		this.icon = icon;
-		this.price = price;
-		this.quantity = quantity;
-		this.description = description;
-		this.minLevel = minLevel;
-		this.skills = skills;
-		this.developers = developers;
-	}
+    public Artefact(Long id, String name, String icon, Long price, Long quantity, String description, Level minLevel,
+            List<Skill> skills, List<Developer> developers) {
+        this.id = id;
+        this.name = name;
+        this.icon = icon;
+        this.price = price;
+        this.quantity = quantity;
+        this.description = description;
+        this.minLevel = minLevel;
+        this.skills = skills;
+        this.developers = developers;
+    }
 
-	public Long getId() {
+    public Long getId() {
         return id;
     }
 
@@ -145,40 +155,58 @@ public class Artefact {
         this.minLevel = minLevel;
     }
 
-	public Long getQuantity() {
-		return quantity;
-	}
+    public Long getQuantity() {
+        return quantity;
+    }
 
-	public void setQuantity(Long quantity) {
-		this.quantity = quantity;
-	}
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
-    
-	@Override
-	public boolean equals(Object obj) {
-	    if (obj == null) {
-	        return false;
-	    }
-	    if (!Artefact.class.isAssignableFrom(obj.getClass())) {
-	        return false;
-	    }
-	    final Artefact other = (Artefact) obj;
-	    if ((this.name == null) ? (other.name != null) : !this.name.equals(other.name)) {
-	        return false;
-	    }
-	    if (this.id != other.id) {
-	        return false;
-	    }
-	    return true;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        return result;
+    }
 
-    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Artefact other = (Artefact) obj;
+        if (id == null) {
+            if (other.id != null) {
+                return false;
+            }
+        } else if (!id.equals(other.id)) {
+            return false;
+        }
+        if (name == null) {
+            if (other.name != null) {
+                return false;
+            }
+        } else if (!name.equals(other.name)) {
+            return false;
+        }
+        return true;
+    }
+
 }
