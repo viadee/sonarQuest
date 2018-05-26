34e8112740d110fb108680644269b612a2b73e9e
Achim Abeling <achimabeling@web.de>
2018-05-26 19:20:39 +0200

    added i18n for de

--- sonarQuest-frontend/src/app/app.component.ts
+++ sonarQuest-frontend/src/app/app.component.ts
@@ -35,8 +35,8 @@ export class AppComponent {
     public translate: TranslateService,
     private dialog: MatDialog) {
 
-    translate.setDefaultLang('en'); // this language will be used as a fallback when a translation isn't found in the current language
-    translate.use('en'); // the lang to use, if the lang isn't available, it will use the current loader to get them    ,
+    translate.setDefaultLang('de'); // this language will be used as a fallback when a translation isn't found in the current language
+    translate.use('de'); // the lang to use, if the lang isn't available, it will use the current loader to get them    ,
 
     
 
