690967d15462c4541b4d34249cae1c0dbcbcafb7
Meschede <Bjoern.Meschede@viadee.de>
2018-07-02 15:08:19 +0200

    Refactoring: Deleted SimulatedSonarQubeServer
    Refactoring: Deleted SimulatedSonarQubeServer

--- sonarQuest-backend/src/main/java/com/viadee/sonarQuest/services/ExternalRessourceService.java
+++ sonarQuest-backend/src/main/java/com/viadee/sonarQuest/services/ExternalRessourceService.java
@@ -1,55 +1,139 @@
 package com.viadee.sonarQuest.services;
 
+import java.net.ConnectException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
 
+import com.viadee.sonarQuest.entities.SonarConfig;
 import com.viadee.sonarQuest.entities.StandardTask;
 import com.viadee.sonarQuest.entities.World;
 import com.viadee.sonarQuest.externalRessources.SonarQubeIssue;
+import com.viadee.sonarQuest.externalRessources.SonarQubeIssueRessource;
+import com.viadee.sonarQuest.externalRessources.SonarQubePaging;
 import com.viadee.sonarQuest.externalRessources.SonarQubeProject;
+import com.viadee.sonarQuest.externalRessources.SonarQubeProjectRessource;
 import com.viadee.sonarQuest.rules.SonarQubeStatusMapper;
 import com.viadee.sonarQuest.rules.SonarQuestStatus;
 
 /**
- * Actual implementation is either {@link RealExternalRessourceService} or {@link SimulatedExternalRessourceService},
- * depending on the command line property simulateSonarServer
+ * Service to access SonarQube server.
  */
-public abstract class ExternalRessourceService {
+@Service
+public class ExternalRessourceService {
 
-    @Autowired
-    private StandardTaskEvaluationService standardTaskEvaluationService;
+	@Autowired
+	private StandardTaskEvaluationService standardTaskEvaluationService;
 
-    @Autowired
-    private SonarQubeStatusMapper statusMapper;
+	@Autowired
+	private SonarQubeStatusMapper statusMapper;
 
-    public abstract List<SonarQubeProject> getSonarQubeProjects();
+	@Autowired
+	private SonarConfigService sonarConfigService;
 
-    public List<World> generateWorldsFromSonarQubeProjects() {
-        return getSonarQubeProjects().stream().map(this::toWorld).collect(Collectors.toList());
-    }
+	@Autowired
+	private RestTemplateService restTemplateService;
 
-    public World toWorld(final SonarQubeProject sonarQubeProject) {
-        return new World(sonarQubeProject.getName(), sonarQubeProject.getKey(), false);
-    }
+	private static final Logger LOGGER = LoggerFactory.getLogger(ExternalRessourceService.class);
 
-    public abstract List<SonarQubeIssue> getIssuesForSonarQubeProject(String projectKey);
+	private static final String ERROR_NO_CONNECTION = "No connection to backend - please adjust the url to the sonar server or start this server with --simulateSonarServer=true";
 
-    public List<StandardTask> generateStandardTasksFromSonarQubeIssuesForWorld(final World world) {
-        final List<SonarQubeIssue> sonarQubeIssues = getIssuesForSonarQubeProject(world.getProject());
-        return sonarQubeIssues.stream().map(sonarQubeIssue -> toTask(sonarQubeIssue, world))
-                .collect(Collectors.toList());
-    }
+	public List<World> generateWorldsFromSonarQubeProjects() {
+		return getSonarQubeProjects().stream().map(this::toWorld).collect(Collectors.toList());
+	}
 
-    public StandardTask toTask(final SonarQubeIssue sonarQubeIssue, final World world) {
-        final Long gold = standardTaskEvaluationService.evaluateGoldAmount(sonarQubeIssue.getDebt());
-        final Long xp = standardTaskEvaluationService.evaluateXP(sonarQubeIssue.getSeverity());
-        final Integer debt = Math.toIntExact(standardTaskEvaluationService.getDebt(sonarQubeIssue.getDebt()));
-        final SonarQuestStatus status = statusMapper.mapExternalStatus(sonarQubeIssue);
-        return new StandardTask(sonarQubeIssue.getMessage(), status.getText(), gold, xp, null, world,
-                sonarQubeIssue.getKey(), sonarQubeIssue.getComponent(), sonarQubeIssue.getSeverity(),
-                sonarQubeIssue.getType(), debt, sonarQubeIssue.getKey());
-    }
+	public World toWorld(final SonarQubeProject sonarQubeProject) {
+		return new World(sonarQubeProject.getName(), sonarQubeProject.getKey(), false);
+	}
+
+	public List<StandardTask> generateStandardTasksFromSonarQubeIssuesForWorld(final World world) {
+		final List<SonarQubeIssue> sonarQubeIssues = getIssuesForSonarQubeProject(world.getProject());
+		return sonarQubeIssues.stream().map(sonarQubeIssue -> toTask(sonarQubeIssue, world))
+				.collect(Collectors.toList());
+	}
+
+	public StandardTask toTask(final SonarQubeIssue sonarQubeIssue, final World world) {
+		final Long gold = standardTaskEvaluationService.evaluateGoldAmount(sonarQubeIssue.getDebt());
+		final Long xp = standardTaskEvaluationService.evaluateXP(sonarQubeIssue.getSeverity());
+		final Integer debt = Math.toIntExact(standardTaskEvaluationService.getDebt(sonarQubeIssue.getDebt()));
+		final SonarQuestStatus status = statusMapper.mapExternalStatus(sonarQubeIssue);
+		return new StandardTask(sonarQubeIssue.getMessage(), status.getText(), gold, xp, null, world,
+				sonarQubeIssue.getKey(), sonarQubeIssue.getComponent(), sonarQubeIssue.getSeverity(),
+				sonarQubeIssue.getType(), debt, sonarQubeIssue.getKey());
+	}
+
+	public List<SonarQubeProject> getSonarQubeProjects() {
+		try {
+			final SonarConfig sonarConfig = sonarConfigService.getConfig();
+			final List<SonarQubeProject> sonarQubeProjects = new ArrayList<>();
+			final SonarQubeProjectRessource sonarQubeProjectRessource = getSonarQubeProjecRessourceForPageIndex(sonarConfig, 1);
+
+			sonarQubeProjects.addAll(sonarQubeProjectRessource.getSonarQubeProjects());
+
+			final Integer pagesOfExternalProjects = determinePagesOfExternalRessourcesToBeRequested(sonarQubeProjectRessource.getPaging());
+			for (int i = 2; i <= pagesOfExternalProjects; i++) {
+				sonarQubeProjects.addAll(getSonarQubeProjecRessourceForPageIndex(sonarConfig, i).getSonarQubeProjects());
+			}
+			return sonarQubeProjects;
+		} catch (final Exception e) {
+			LOGGER.error(e.getMessage());
+			throw e;
+		}
+	}
+
+	public List<SonarQubeIssue> getIssuesForSonarQubeProject(final String projectKey) {
+		try {
+			final SonarConfig sonarConfig = sonarConfigService.getConfig();
+			final List<SonarQubeIssue> sonarQubeIssueList = new ArrayList<>();
+			final SonarQubeIssueRessource sonarQubeIssueRessource = getSonarQubeIssueResourceForProjectAndPageIndex(
+					sonarConfig, projectKey, 1);
+
+			sonarQubeIssueList.addAll(sonarQubeIssueRessource.getIssues());
+
+			final Integer pagesOfExternalIssues = determinePagesOfExternalRessourcesToBeRequested(
+					sonarQubeIssueRessource.getPaging());
+			for (int i = 2; i <= pagesOfExternalIssues; i++) {
+				sonarQubeIssueList.addAll(getSonarQubeIssueResourceForProjectAndPageIndex(sonarConfig, projectKey, i).getIssues());
+			}
+			return sonarQubeIssueList;
+		} catch (final ResourceAccessException e) {
+			if (e.getCause() instanceof ConnectException) {
+				LOGGER.error(ERROR_NO_CONNECTION);
+			}
+			throw e;
+		}
+	}
+
+	public int determinePagesOfExternalRessourcesToBeRequested(final SonarQubePaging sonarQubePaging) {
+		return sonarQubePaging.getTotal() / sonarQubePaging.getPageSize() + 1;
+	}
+
+	private SonarQubeIssueRessource getSonarQubeIssueResourceForProjectAndPageIndex(final SonarConfig sonarConfig,
+			final String projectKey, final int pageIndex) {
+		final RestTemplate restTemplate = restTemplateService.getRestTemplate(sonarConfig);
+		final String fooResourceUrl = sonarConfig.getSonarServerUrl() + "/api/issues/search?componentRoots="
+				+ projectKey + "&pageSize=500&pageIndex=" + pageIndex;
+		final ResponseEntity<SonarQubeIssueRessource> response = restTemplate.getForEntity(fooResourceUrl,
+				SonarQubeIssueRessource.class);
+		return response.getBody();
+	}
+
+	private SonarQubeProjectRessource getSonarQubeProjecRessourceForPageIndex(final SonarConfig sonarConfig,
+			final int pageIndex) {
+		final RestTemplate restTemplate = restTemplateService.getRestTemplate(sonarConfig);
+		final String fooResourceUrl = sonarConfig.getSonarServerUrl()
+				+ "/api/components/search?qualifiers=TRK&pageSize=500&pageIndex=" + pageIndex;
+		final ResponseEntity<SonarQubeProjectRessource> response = restTemplate.getForEntity(fooResourceUrl,
+				SonarQubeProjectRessource.class);
+		return response.getBody();
+	}
 
 }
