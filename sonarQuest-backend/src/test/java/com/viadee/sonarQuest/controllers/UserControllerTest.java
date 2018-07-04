6c51056ad35d2d6e14ba6ab3dd79e61c7387bb30
Christoph-Meyer <mec@viadee.de>
2018-07-04 15:46:06 +0200

    Test for loading avatars

---
+++ sonarQuest-backend/src/test/java/com/viadee/sonarQuest/controllers/UserControllerTest.java
@@ -0,0 +1,54 @@
+package com.viadee.sonarQuest.controllers;
+
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.security.Principal;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import com.viadee.sonarQuest.entities.User;
+import com.viadee.sonarQuest.services.UserService;
+
+@RunWith(MockitoJUnitRunner.class)
+public class UserControllerTest {
+
+    @Mock
+    private UserService userService;
+
+    @InjectMocks
+    private UserController userController;
+
+    @Test
+    public void testAvatar_userServiceFindetUserNicht() throws Exception {
+        Principal principal = mock(Principal.class);
+        when(principal.getName()).thenReturn("Aria");
+        when(userService.findByUsername("Aria")).thenReturn(null);
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        byte[] avatar = userController.avatar(principal, response);
+        assertNull(avatar);
+    }
+
+    @Test
+    public void testAvatar_userServiceMitUserOhnePicture() throws Exception {
+        Principal principal = mock(Principal.class);
+        when(principal.getName()).thenReturn("Aria");
+        User user = mock(User.class);
+        when(user.getPicture()).thenReturn(null);
+        when(userService.findByUsername("Aria")).thenReturn(user);
+        ReflectionTestUtils.setField(userController, "avatarDirectoryPath",
+                ".");
+        HttpServletResponse response = mock(HttpServletResponse.class);
+        byte[] avatar = userController.avatar(principal, response);
+        assertNull(avatar);
+    }
+
+}
