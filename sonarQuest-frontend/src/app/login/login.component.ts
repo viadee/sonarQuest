f2b391227cc218a761194bb87a645fc52a9e75c0
Hinne Stolzenberg <hinne.stolzenberg@md.de>
2018-07-05 08:13:36 +0200

    fix environment variables - change login dialog to be more intuitive

--- sonarQuest-frontend/src/app/login/login.component.ts
+++ sonarQuest-frontend/src/app/login/login.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {UsernamePassword} from './username-password';
+import { UserService } from '../services/user.service';
+import { AuthenticationService } from './authentication.service';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +14,7 @@ export class LoginComponent implements OnInit {
 
   loginFormGroup: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private authService: AuthenticationService, private dialog: MatDialog) {
   }
 
   ngOnInit() {
@@ -21,5 +24,11 @@ export class LoginComponent implements OnInit {
         password: ''
       }
     );
+  }
+
+  onSubmit() {
+    console.log(this.loginFormGroup.value);
+    this.authService.login(this.loginFormGroup.value.username, this.loginFormGroup.value.password);
+    this.dialog.closeAll();
   }
 }
--- sonarQuest-frontend/src/environments/environment.prod.ts
+++ sonarQuest-frontend/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  endpoint: 'http://ittc-2.mobilcom.de:8081',
 };
