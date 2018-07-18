39d7fafbb22d281f13afdd7c819f1873eb99d316
MeC <MeC@NBWXW484.intern.viadee.de>
2018-07-18 19:34:12 +0200

    Basic set of 12 avatars added

---
+++ sonarQuest-backend/src/main/resources/db/data/V0_0_4_1__avatars.sql
@@ -0,0 +1,155 @@
+INSERT INTO User (username, password, role_id, gold, xp, level_id, picture, about_me, avatar_class_id, avatar_race_id) 
+ VALUES (
+ 'Kron',   
+ '$2a$10$LoXVU5ODwytMz3Mh/Nft4.WaasCtwEuN6NEeJCER5X8o1ayCJHVxO', 
+ 2 /* DEV */, 
+ 0, 
+ 0, 
+ 1 /* Level */, 
+ 'ava_barbarian1.jpg', 
+ 'The mysterious yet totally ineffective barbarian who just needs someone to awaken their soft side.', 
+ 2 /* Warrior */, 
+ 3 /* Barbarian */);
+
+INSERT INTO User (username, password, role_id, gold, xp, level_id, picture, about_me, avatar_class_id, avatar_race_id) 
+ VALUES (
+ 'Bingo',   
+ '$2a$10$LoXVU5ODwytMz3Mh/Nft4.WaasCtwEuN6NEeJCER5X8o1ayCJHVxO', 
+ 2 /* DEV */, 
+ 0, 
+ 0, 
+ 1 /* Level */, 
+ 'ava_bard1.jpg', 
+ 'The bard who steals from the rich and gives to the poor.', 
+ 5 /* Bard */, 
+ 1 /* Human */);
+ 
+INSERT INTO User (username, password, role_id, gold, xp, level_id, picture, about_me, avatar_class_id, avatar_race_id) 
+ VALUES (
+ 'Ravurv',   
+ '$2a$10$LoXVU5ODwytMz3Mh/Nft4.WaasCtwEuN6NEeJCER5X8o1ayCJHVxO', 
+ 2 /* DEV */, 
+ 0, 
+ 0, 
+ 1 /* Level */, 
+ 'ava_druid1.jpg', 
+ 'The wise guide who has to be better than anyone else.', 
+ 7 /* Druid */, 
+ 1 /* Human */);
+ 
+INSERT INTO User (username, password, role_id, gold, xp, level_id, picture, about_me, avatar_class_id, avatar_race_id) 
+ VALUES (
+ 'Ghilanna',   
+ '$2a$10$LoXVU5ODwytMz3Mh/Nft4.WaasCtwEuN6NEeJCER5X8o1ayCJHVxO', 
+ 2 /* DEV */, 
+ 0, 
+ 0, 
+ 1 /* Level */, 
+ 'ava_elf4.jpg', 
+ 'The friendly elven warrior who works as an assassin against the forces of darkness.', 
+ 2 /* Warrior */, 
+ 4 /* Elf */);
+ 
+INSERT INTO User (username, password, role_id, gold, xp, level_id, picture, about_me, avatar_class_id, avatar_race_id) 
+ VALUES (
+ 'Clawbender',   
+ '$2a$10$LoXVU5ODwytMz3Mh/Nft4.WaasCtwEuN6NEeJCER5X8o1ayCJHVxO', 
+ 2 /* DEV */, 
+ 0, 
+ 0, 
+ 1 /* Level */, 
+ 'ava_fighter2.jpg', 
+ 'The heir to greatness raised in secret who seeks to avenge the murder of his family.', 
+ 2 /* Warrior */, 
+ 1 /* Human */);
+ 
+ INSERT INTO User (username, password, role_id, gold, xp, level_id, picture, about_me, avatar_class_id, avatar_race_id) 
+ VALUES (
+ 'Heribald',   
+ '$2a$10$LoXVU5ODwytMz3Mh/Nft4.WaasCtwEuN6NEeJCER5X8o1ayCJHVxO', 
+ 2 /* DEV */, 
+ 0, 
+ 0, 
+ 1 /* Level */, 
+ 'ava_hobbit1.jpg', 
+ 'The scoundrel with a heart of gold who is just this side of crazy.', 
+ 3 /* Adventurer */, 
+ 5 /* Hobbit */);
+ 
+ INSERT INTO User (username, password, role_id, gold, xp, level_id, picture, about_me, avatar_class_id, avatar_race_id) 
+ VALUES (
+ 'Reimfred',   
+ '$2a$10$LoXVU5ODwytMz3Mh/Nft4.WaasCtwEuN6NEeJCER5X8o1ayCJHVxO', 
+ 2 /* DEV */, 
+ 0, 
+ 0, 
+ 1 /* Level */, 
+ 'ava_knight3.jpg', 
+ 'The loudmouthed opportunist with a heart of gold who fights crime as a vigilante.', 
+ 8 /* Knight */, 
+ 1 /* Human */); 
+
+ INSERT INTO User (username, password, role_id, gold, xp, level_id, picture, about_me, avatar_class_id, avatar_race_id) 
+ VALUES (
+ 'Hildegor',   
+ '$2a$10$LoXVU5ODwytMz3Mh/Nft4.WaasCtwEuN6NEeJCER5X8o1ayCJHVxO', 
+ 2 /* DEV */, 
+ 0, 
+ 0, 
+ 1 /* Level */, 
+ 'ava_knight7.jpg', 
+ 'The adventurous lord who knows the solution to all problems.', 
+ 8 /* Knight */, 
+ 1 /* Human */); 
+ 
+ INSERT INTO User (username, password, role_id, gold, xp, level_id, picture, about_me, avatar_class_id, avatar_race_id) 
+ VALUES (
+ 'Zefarihm',   
+ '$2a$10$LoXVU5ODwytMz3Mh/Nft4.WaasCtwEuN6NEeJCER5X8o1ayCJHVxO', 
+ 2 /* DEV */, 
+ 0, 
+ 0, 
+ 1 /* Level */, 
+ 'ava_mage5.jpg', 
+ 'The awkward, greedy teacher who can not resist helping people.', 
+ 1 /* Mage */, 
+ 1 /* Human */);  
+ 
+ INSERT INTO User (username, password, role_id, gold, xp, level_id, picture, about_me, avatar_class_id, avatar_race_id) 
+ VALUES (
+ 'Grimace',   
+ '$2a$10$LoXVU5ODwytMz3Mh/Nft4.WaasCtwEuN6NEeJCER5X8o1ayCJHVxO', 
+ 2 /* DEV */, 
+ 0, 
+ 0, 
+ 1 /* Level */, 
+ 'ava_scoundrel1.jpg', 
+ 'The dexterous, unjust murderer who seems insane but tries to protect his family.', 
+ 4 /* Thief */, 
+ 1 /* Human */);   
+ 
+ INSERT INTO User (username, password, role_id, gold, xp, level_id, picture, about_me, avatar_class_id, avatar_race_id) 
+ VALUES (
+ 'Rawena',   
+ '$2a$10$LoXVU5ODwytMz3Mh/Nft4.WaasCtwEuN6NEeJCER5X8o1ayCJHVxO', 
+ 2 /* DEV */, 
+ 0, 
+ 0, 
+ 1 /* Level */, 
+ 'ava_witch1.jpg', 
+ 'The witch who wanders the world righting wrongs.', 
+ 6 /* Witch */, 
+ 1 /* Human */);   
+
+INSERT INTO User (username, password, role_id, gold, xp, level_id, picture, about_me, avatar_class_id, avatar_race_id) 
+ VALUES (
+ 'Tolana',   
+ '$2a$10$LoXVU5ODwytMz3Mh/Nft4.WaasCtwEuN6NEeJCER5X8o1ayCJHVxO', 
+ 2 /* DEV */, 
+ 0, 
+ 0, 
+ 1 /* Level */, 
+ 'ava_fighter1.jpg', 
+ 'The brave lady of low origin who tries to make a fortune.', 
+ 2 /* Warrior */, 
+ 1 /* Human */); 
