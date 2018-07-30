3618770a6476753f1c36680cb952622caa35e610
Christoph-Meyer <mec@viadee.de>
2018-07-30 11:20:02 +0200

    Test imports corrected

--- sonarQuest-backend/src/test/java/com/viadee/sonarQuest/services/SonarConfigServiceTest.java
+++ sonarQuest-backend/src/test/java/com/viadee/sonarQuest/services/SonarConfigServiceTest.java
@@ -1,28 +1,25 @@
 package com.viadee.sonarQuest.services;
 
-import com.viadee.sonarQuest.SonarQuestApplication;
-import com.viadee.sonarQuest.entities.SonarConfig;
-import com.viadee.sonarQuest.externalRessources.SonarQubeApiResponse;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static junit.framework.TestCase.assertFalse;
-import static junit.framework.TestCase.assertTrue;
+import com.viadee.sonarQuest.entities.SonarConfig;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SonarConfigServiceTest {
 
-
     @Autowired
     SonarConfigService sonarConfigService;
 
     @Test
-    public void testSonarQubeServerURLCheck(){
+    public void testSonarQubeServerURLCheck() {
         SonarConfig sonarConfig = new SonarConfig();
         sonarConfig.setSonarServerUrl("https://www.sonarcloud.io");
         assertTrue(sonarConfigService.checkSonarQubeURL(sonarConfig));
