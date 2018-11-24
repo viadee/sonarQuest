d95b9ebfec6a13f4a3121e11265a09796d6c3555
Christoph-Meyer <mec@viadee.de>
2018-11-24 22:12:48 +0100

    CleanUp

--- sonarQuest-backend/src/main/java/com/viadee/sonarQuest/controllers/PathConstants.java
+++ sonarQuest-backend/src/main/java/com/viadee/sonarQuest/controllers/PathConstants.java
@@ -5,4 +5,8 @@ public final class PathConstants {
     public static final String LOGIN_URL = "/login";
 
     public static final String USER_URL = "/user";
+
+    private PathConstants() {
+        // NOOP
+    }
 }
--- sonarQuest-backend/src/main/java/com/viadee/sonarQuest/services/WizardService.java
+++ sonarQuest-backend/src/main/java/com/viadee/sonarQuest/services/WizardService.java
@@ -2,8 +2,6 @@ package com.viadee.sonarQuest.services;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +11,6 @@ import com.viadee.sonarQuest.entities.World;
 
 @Service
 public class WizardService {
-
-	private static final Logger LOGGER = LoggerFactory.getLogger(WizardService.class);
 
 	@Autowired
 	private WorldService worldService;
