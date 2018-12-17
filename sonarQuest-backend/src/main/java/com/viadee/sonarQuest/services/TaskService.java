ecd5392324114376879f32bb3b9e9f25007e6e09
Christoph Meyer <mec@viadee.de>
2018-12-17 08:46:16 +0100

    Simple fix for issue #99

--- sonarQuest-backend/src/main/java/com/viadee/sonarQuest/services/TaskService.java
+++ sonarQuest-backend/src/main/java/com/viadee/sonarQuest/services/TaskService.java
@@ -19,65 +19,65 @@ import com.viadee.sonarQuest.rules.SonarQuestStatus;
 @Service
 public class TaskService {
 
-    @Autowired
-    private TaskRepository taskRepository;
+	private static final Logger LOGGER = LoggerFactory.getLogger(TaskService.class);
 
-    @Autowired
-    private GratificationService gratificationService;
+	@Autowired
+	private TaskRepository taskRepository;
 
-    @Autowired
-    private QuestService questService;
+	@Autowired
+	private GratificationService gratificationService;
 
-    @Autowired
-    private AdventureService adventureService;
+	@Autowired
+	private QuestService questService;
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(TaskService.class);
+	@Autowired
+	private AdventureService adventureService;
 
-    public List<Task> getFreeTasksForWorld(final World world) {
-        return taskRepository.findByWorldAndStatusAndQuestIsNull(world, SonarQuestStatus.OPEN);
-    }
+	public List<Task> getFreeTasksForWorld(final World world) {
+		return taskRepository.findByWorldAndStatusAndQuestIsNull(world, SonarQuestStatus.OPEN);
+	}
 
-    public Task save(final Task task) {
-        return taskRepository.save(task);
-    }
+	public Task save(final Task task) {
+		return taskRepository.save(task);
+	}
 
-    public Task find(final Long id) {
-        return taskRepository.findById(id);
-    }
+	public Task find(final Long id) {
+		return taskRepository.findById(id);
+	}
 
-    public void delete(final Task task) {
-        taskRepository.delete(task);
-    }
+	public void delete(final Task task) {
+		taskRepository.delete(task);
+	}
 
-    public List<Task> findAll() {
-        return taskRepository.findAll();
-    }
+	public List<Task> findAll() {
+		return taskRepository.findAll();
+	}
 
-    @Transactional
-    public void solveTaskManually(final Task task) {
-        if (task != null && task.getStatus() != SonarQuestStatus.SOLVED) {
-            task.setStatus(SonarQuestStatus.SOLVED);
-            task.setEnddate(new Date(System.currentTimeMillis()));
-            save(task);
-            gratificationService.rewardUserForSolvingTask(task);
-            questService.updateQuest(task.getQuest());
-            adventureService.updateAdventure(task.getQuest().getAdventure());
-        }
-    }
+	@Transactional
+	public void solveTaskManually(final Task task) {
+		if (task != null && task.getStatus() != SonarQuestStatus.SOLVED) {
+			task.setStatus(SonarQuestStatus.SOLVED);
+			task.setEnddate(new Date(System.currentTimeMillis()));
+			save(task);
+			gratificationService.rewardUserForSolvingTask(task);
+			questService.updateQuest(task.getQuest());
+			adventureService.updateAdventure(task.getQuest().getAdventure());
+		}
+	}
 
-    @Transactional
-    public void solveAllTasksInQuest(Quest quest) {
-        if (quest != null && quest.getStatus() != QuestState.SOLVED) {
-            LOGGER.info("Solving all tasks in quest with ID " + quest.getId());
-            List<Task> tasks = quest.getTasks();
-            for (Task task : tasks) {
-                gratificationService.rewardUserForSolvingTask(task);
-                task.setStatus(SonarQuestStatus.SOLVED);
-                task.setEnddate(new Date(System.currentTimeMillis()));
-                save(task);
-            }
-            questService.updateQuest(quest);
-            adventureService.updateAdventure(quest.getAdventure());
-        }
-    }
+	@Transactional
+	public void solveAllTasksInQuest(Quest quest) {
+		if (quest != null && quest.getStatus() != QuestState.SOLVED) {
+			LOGGER.info("Solving all tasks in quest with ID " + quest.getId());
+			List<Task> tasks = quest.getTasks();
+			for (Task task : tasks) {
+				gratificationService.rewardUserForSolvingTask(task);
+				task.setStatus(SonarQuestStatus.SOLVED);
+				task.setEnddate(new Date(System.currentTimeMillis()));
+				save(task);
+			}
+			questService.updateQuest(quest);
+			adventureService.updateAdventure(quest.getAdventure());
+		}
+	}
 }
--- sonarQuest-backend/src/main/java/com/viadee/sonarQuest/services/UserService.java
+++ sonarQuest-backend/src/main/java/com/viadee/sonarQuest/services/UserService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -23,113 +25,120 @@ import com.viadee.sonarQuest.repositories.UserRepository;
 @Service
 public class UserService implements UserDetailsService {
 
-    @Autowired
-    private RoleService roleService;
+	private static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
 
-    @Autowired
-    private UserRepository userRepository;
+	@Autowired
+	private RoleService roleService;
 
-    @Autowired
-    private WorldService worldService;
+	@Autowired
+	private UserRepository userRepository;
 
-    @Autowired
-    private LevelService levelService;
+	@Autowired
+	private WorldService worldService;
 
-    @Autowired
-    private PermissionService permissionService;
+	@Autowired
+	private LevelService levelService;
 
-    @Override
-    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
-        final User user = findByUsername(username);
-        final Set<Permission> permissions = permissionService.getAccessPermissions(user);
+	@Autowired
+	private PermissionService permissionService;
 
-        final List<SimpleGrantedAuthority> authoritys = permissions
-                .stream()
-                .map(berechtigung -> new SimpleGrantedAuthority(berechtigung.getPermission()))
-                .collect(Collectors.toList());
+	@Override
+	public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException {
+		final User user = findByUsername(username);
+		final Set<Permission> permissions = permissionService.getAccessPermissions(user);
 
-        return new org.springframework.security.core.userdetails.User(
-                user.getUsername(), user.getPassword(), true, true, true, true, authoritys);
-    }
+		final List<SimpleGrantedAuthority> authoritys = permissions.stream()
+				.map(berechtigung -> new SimpleGrantedAuthority(berechtigung.getPermission()))
+				.collect(Collectors.toList());
 
-    public User findByUsername(final String username) {
-        return userRepository.findByUsername(username);
-    }
+		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), true,
+				true, true, true, authoritys);
+	}
 
-    private User findById(final Long id) {
-        return userRepository.findOne(id);
-    }
+	public User findByUsername(final String username) {
+		return userRepository.findByUsername(username);
+	}
 
-    public World updateUsersCurrentWorld(final User user, final Long worldId) {
-        final World world = worldService.findById(worldId);
-        user.setCurrentWorld(world);
-        userRepository.saveAndFlush(user);
-        return user.getCurrentWorld();
-    }
+	private User findById(final Long id) {
+		return userRepository.findOne(id);
+	}
 
-    public synchronized User save(final User user) {
-        User toBeSaved = null;
-        String username = user.getUsername();
-        if (user.getId() == null) {
-            // Only the password hash needs to be saved
-            BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-            String password = encoder.encode(user.getPassword());
-            Role role = user.getRole();
-            RoleName roleName = role.getName();
-            Role userRole = roleService.findByName(roleName);
-            toBeSaved = usernameFree(username) ? user : null;
-            if (toBeSaved != null) {
-                toBeSaved.setPassword(password);
-                toBeSaved.setRole(userRole);
-                toBeSaved.setCurrentWorld(user.getCurrentWorld());
-                toBeSaved.setGold(0l);
-                toBeSaved.setXp(0l);
-                toBeSaved.setLevel(levelService.getLevelByUserXp(0l));
-            }
-        } else {
-            toBeSaved = findById(user.getId());
-            if (toBeSaved != null) {
-                if (!username.equals(toBeSaved.getUsername()) && usernameFree(username)) {
-                    toBeSaved.setUsername(username);
-                }
-                toBeSaved.setAboutMe(user.getAboutMe());
-                toBeSaved.setPicture(user.getPicture());
-                toBeSaved.setCurrentWorld(user.getCurrentWorld());
-                toBeSaved.setWorlds(user.getWorlds());
-                toBeSaved.setGold(user.getGold());
-                toBeSaved.setXp(user.getXp());
-                toBeSaved.setLevel(levelService.getLevelByUserXp(user.getXp()));
-            }
-        }
+	public World updateUsersCurrentWorld(final User user, final Long worldId) {
+		final World world = worldService.findById(worldId);
+		user.setCurrentWorld(world);
+		userRepository.saveAndFlush(user);
+		return user.getCurrentWorld();
+	}
 
-        return toBeSaved != null ? userRepository.saveAndFlush(toBeSaved) : null;
-    }
+	public synchronized User save(final User user) {
+		User toBeSaved = null;
+		String username = user.getUsername();
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		if (user.getId() == null) {
+			// Only the password hash needs to be saved
+			String password = encoder.encode(user.getPassword());
+			Role role = user.getRole();
+			RoleName roleName = role.getName();
+			Role userRole = roleService.findByName(roleName);
+			toBeSaved = usernameFree(username) ? user : null;
+			if (toBeSaved != null) {
+				toBeSaved.setPassword(password);
+				toBeSaved.setRole(userRole);
+				toBeSaved.setCurrentWorld(user.getCurrentWorld());
+				toBeSaved.setGold(0l);
+				toBeSaved.setXp(0l);
+				toBeSaved.setLevel(levelService.getLevelByUserXp(0l));
+			}
+		} else {
+			toBeSaved = findById(user.getId());
+			if (toBeSaved != null) {
+				if (!username.equals(toBeSaved.getUsername()) && usernameFree(username)) {
+					toBeSaved.setUsername(username);
+				}
+				// change password only if it differs from the old one
+				String oldPassHash = toBeSaved.getPassword();
+				if (!encoder.matches(user.getPassword(), oldPassHash)) {
+					String password = encoder.encode(user.getPassword());
+					toBeSaved.setPassword(password);
+					LOGGER.info("The password for user " + user.getUsername() + " (id: " + user.getId()
+							+ ") has been changed.");
+				}
+				toBeSaved.setAboutMe(user.getAboutMe());
+				toBeSaved.setPicture(user.getPicture());
+				toBeSaved.setCurrentWorld(user.getCurrentWorld());
+				toBeSaved.setWorlds(user.getWorlds());
+				toBeSaved.setGold(user.getGold());
+				toBeSaved.setXp(user.getXp());
+				toBeSaved.setLevel(levelService.getLevelByUserXp(user.getXp()));
+			}
+		}
 
-    private boolean usernameFree(final String username) {
-        return userRepository.findByUsername(username) == null;
-    }
+		return toBeSaved != null ? userRepository.saveAndFlush(toBeSaved) : null;
+	}
 
-    public void delete(final Long userId) {
-        final User user = findById(userId);
-        userRepository.delete(user);
-    }
+	private boolean usernameFree(final String username) {
+		return userRepository.findByUsername(username) == null;
+	}
 
-    public User findById(final long userId) {
-        return userRepository.findOne(userId);
-    }
+	public void delete(final Long userId) {
+		final User user = findById(userId);
+		userRepository.delete(user);
+	}
 
-    public List<User> findAll() {
-        return userRepository.findAll();
-    }
+	public User findById(final long userId) {
+		return userRepository.findOne(userId);
+	}
 
-    public List<User> findByRole(final RoleName roleName) {
-        return findAll().stream()
-                .filter(user -> user.getRole().getName() == roleName)
-                .collect(Collectors.toList());
-    }
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
 
-    public Level getLevel(final long xp) {
-        return levelService.getLevelByUserXp(xp);
-    }
+	public List<User> findByRole(final RoleName roleName) {
+		return findAll().stream().filter(user -> user.getRole().getName() == roleName).collect(Collectors.toList());
+	}
+
+	public Level getLevel(final long xp) {
+		return levelService.getLevelByUserXp(xp);
+	}
 
 }
