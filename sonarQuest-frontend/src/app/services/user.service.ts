7fb8d064cbf54d1216a3efc735e1e9390638b84b
Christopher Brown <christopher.brown@viadee.de>
2019-03-05 09:38:09 +0100

    Fixes Issue #151

--- sonarQuest-frontend/src/app/services/user.service.ts
+++ sonarQuest-frontend/src/app/services/user.service.ts
@@ -4,7 +4,8 @@ import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
 import {Observable, Subscriber} from 'rxjs';
 import {AuthenticationService} from '../login/authentication.service';
-import {map, tap} from 'rxjs/operators';
+import {tap} from 'rxjs/operators';
+import * as moment from 'moment';
 
 @Injectable()
 export class UserService {
@@ -53,7 +54,7 @@ export class UserService {
     const url = `${environment.endpoint}/user/all`;
     return this.httpClient.get <User[]>(url).pipe(tap((users: User[]) => {
       users.forEach(user =>
-        user.lastLogin = user.lastLogin ? new Date(user.lastLogin).toTimeString() : null
+        user.lastLogin = user.lastLogin ? moment(new Date(user.lastLogin)).format('DD.MM.YYYY HH:mm:ss') : null
       )
     }));
   }
