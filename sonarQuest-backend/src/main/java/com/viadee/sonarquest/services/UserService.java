833d5e4f67307d063a14345680f4a460a78e6758
Christoph-Meyer <mec@viadee.de>
2019-03-05 09:03:30 +0100

    Import fixed

--- sonarQuest-backend/src/main/java/com/viadee/sonarquest/services/UserService.java
+++ sonarQuest-backend/src/main/java/com/viadee/sonarquest/services/UserService.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
