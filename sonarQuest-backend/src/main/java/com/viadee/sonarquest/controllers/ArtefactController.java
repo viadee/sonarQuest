04a0dc4e291d7ecc50ea844d9785343a5fe1ebeb
Nils <nils.uhrberg@viadee.de>
2019-04-30 11:24:35 +0200

    Removed unused imports

--- sonarQuest-backend/src/main/java/com/viadee/sonarquest/controllers/ArtefactController.java
+++ sonarQuest-backend/src/main/java/com/viadee/sonarquest/controllers/ArtefactController.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
