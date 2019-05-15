f65a670eaa3042b6794b121cff9e756f488ac9ea
Michael Landreh <michael.landreh@gmail.com>
2019-05-15 13:51:21 +0200

    Fixed frontend testing issues and added public fields to event mock service

--- sonarQuest-frontend/src/app/pages/gamemaster-page/components/gamemaster-marketplace/components/gamemaster-artefact-edit/components/gamemaster-skill-edit/gamemaster-skill-edit.component.spec.ts
+++ sonarQuest-frontend/src/app/pages/gamemaster-page/components/gamemaster-marketplace/components/gamemaster-artefact-edit/components/gamemaster-skill-edit/gamemaster-skill-edit.component.spec.ts
@@ -1,14 +1,60 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GamemasterSkillEditComponent } from './gamemaster-skill-edit.component';
+import {TranslateTestingModule} from "../../../../../../../../services/translate.service.mock.module";
+import {BrowserModule} from "@angular/platform-browser";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {RouterTestingModule} from "@angular/router/testing";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule, MatDialogRef,
+  MatDividerModule,
+  MatFormFieldModule,
+  MatIconModule, MatInputModule, MatSelectModule,
+  MatToolbarModule,
+  MatTooltipModule
+} from "@angular/material";
+import {CovalentDataTableModule} from "@covalent/core";
+import {ArtefactServiceTestingModule} from "../../../../../../../../services/artefact.service.mock.module";
+import {SkillServiceTestingModule} from "../../../../../../../../services/skill.service.mock.module";
 
 describe('GamemasterSkillEditComponent', () => {
   let component: GamemasterSkillEditComponent;
   let fixture: ComponentFixture<GamemasterSkillEditComponent>;
 
+  const data = {
+    minLevel: {
+      levelNumber: 1
+    }
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GamemasterSkillEditComponent ]
+      declarations: [ GamemasterSkillEditComponent ],
+      imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        RouterTestingModule,
+        FormsModule,
+        ReactiveFormsModule,
+        TranslateTestingModule,
+        MatTooltipModule,
+        MatIconModule,
+        MatDialogModule,
+        MatSelectModule,
+        MatDividerModule,
+        MatToolbarModule,
+        MatFormFieldModule,
+        MatInputModule,
+        CovalentDataTableModule,
+        ArtefactServiceTestingModule,
+        SkillServiceTestingModule
+      ],
+      providers: [
+        {provide: MAT_DIALOG_DATA, useValue: data},
+        {provide: MatDialogRef, useValue: {}}
+      ]
     })
     .compileComponents();
   }));
--- sonarQuest-frontend/src/app/services/event.service.mock.module.ts
+++ sonarQuest-frontend/src/app/services/event.service.mock.module.ts
@@ -2,11 +2,17 @@ import {Injectable, NgModule} from '@angular/core';
 import {EventService} from "./event.service";
 import {Observable} from "rxjs";
 import {Event} from "../Interfaces/Event";
+import {EventDto} from "../Interfaces/EventDto";
+import {UserDto} from "../Interfaces/UserDto";
+import {EventUserDto} from "../Interfaces/EventUserDto";
 
 @Injectable()
 export class EventServiceMock {
 
   public  events$ = new Observable<Event[]>();
+  public  eventUserDto$ = new Observable<EventUserDto>();
+  public  eventDtos$ = new Observable<EventDto[]>();
+  public  userDtos$  = new Observable<UserDto[]>();
 
   getEventsOfCurrentWorld(): Observable<Event[]>{
     return new Observable<Event[]>();
