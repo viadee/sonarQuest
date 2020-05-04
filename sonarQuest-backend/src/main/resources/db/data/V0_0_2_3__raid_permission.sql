c09b0f1f5790b5c522b1400da4456ee225181e49
kalinow <m.kalinowski5@gmx.de>
2020-05-04 19:29:18 +0200

    raid permission outsourced in own sql file

--- sonarQuest-backend/src/main/resources/db/data/V0_0_2_3__raid_permission.sql
+++ sonarQuest-backend/src/main/resources/db/data/V0_0_2_3__raid_permission.sql
@@ -1,7 +1,9 @@
 INSERT INTO Permission (id, type, permission) VALUES
- (12, 'URL', 'events');
+ (15, 'URL', 'raids' ),
+ (16, 'URL', 'raid' ),
+ (17, 'URL', 'qualitygate' );
  
  INSERT INTO Role_To_Permission (role_id, permission_id) VALUES 
- (1,12),
- (2,12),
- (3,12);
+ (2,15),
+ (2,16),
+ (2,17);
